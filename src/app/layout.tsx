393ff8c06f6b58a2994e49d2106ff865c5f6e530
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2025-04-11 10:39:28 +0700

    chore: add GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,10 +70,24 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="canonical" href="https://prompts.opentyphoon.ai" />
         <meta name="robots" content="index, follow" />
+        <Script>
+          {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-WK925XWL');
+    `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
       >
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WK925XWL"
+            height={0} width={0} style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
+
         <Script id="schema-org" type="application/ld+json">
           {`
             {
